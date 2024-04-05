1fabff11a1cda9c3ccfb3c71058958abf4a1865f
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-04-05 17:26:11 -0400

    feat: test api using JSON ouput.

--- test_openai.py
+++ test_openai.py
@@ -20,30 +20,34 @@ openai.api_key = api_key
 
 # Create a chat completion
 completion = openai.chat.completions.create(
-    model="gpt-3.5-turbo",
+    model="gpt-3.5-turbo-0125",
+    response_format={"type": "json_object"},
     messages=[
         {
             "role": "system",
-            "content": prompt.system_prompt,
+            "content": prompt.system_prompt + "USE JSON FOR OUTPUT",
         },
         {
             "role": "user",
             "content": prompt.user_reports,
         },
     ],
+    temperature=0.5,
 )
 
 # Print the generated message from the completion
 message_content = completion.choices[0].message.content
 
-# Breaking down the message into lines for better readability
-lines = message_content.split("\n")
+print(message_content)
 
-# Formatting and printing each line with annotations for clarity
-print("\nTicket Summary:\n")
-for line in lines:
-    # Check if the line contains valuable information before printing
-    if line.strip() != "":  # This removes any lines that are just whitespace
-        # Here, you can customize how each line is displayed.
-        # For example, adding "->" before each line item for clarity.
-        print(f"{line}")
+# Breaking down the message into lines for better readability
+# lines = message_content.split("\n")
+
+# # Formatting and printing each line with annotations for clarity
+# print("\nTicket Summary:\n")
+# for line in lines:
+#     # Check if the line contains valuable information before printing
+#     if line.strip() != "":  # This removes any lines that are just whitespace
+#         # Here, you can customize how each line is displayed.
+#         # For example, adding "->" before each line item for clarity.
+#         print(f"{line}")
